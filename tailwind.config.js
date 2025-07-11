a849d477d289b6a000544f3ab0d0ae7ed18b3415
Rahma254 <rahmasiska661@gmail.com>
2025-07-11 19:29:50 +0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// File: tailwind.config.js
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // <-- Ini akan memindai semua file di dalam folder src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
